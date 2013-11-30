9ad8825b2e6f76d6d4819bc6939e689d1140d387
GuuD WU <guuuuud@gmail.com>
2013-11-29 23:00:09 -0500

    Create "gradient.R"
    A private function.
    It computes the gradient of ODE solution with respect to ODE parameters.

---
+++ gradient.R
@@ -0,0 +1,16 @@
+# Compute gradient of ODE system solution with respect to
+# coefficient matrix, intercept and initial condition, respectively.
+
+.gradient <- function (
+  observation
+  , linear_ode
+)
+
+{
+
+# Return#{{{
+
+  return()
+#}}}
+
+}
