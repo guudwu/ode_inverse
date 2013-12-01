0635ae2efc5fbe01f1002eff64027a6d2ed8db29
GuuD WU <guuuuud@gmail.com>
2013-12-01 12:43:52 -0500

    Add "fim"-method block in "variance_linear_ode.R"
    Add input argument "method" and block for value "fim",
    for computing the variance by Fisher Information Matrix.
    
    Create "variance_linear_ode_test.R" in "test/".

---
+++ test/variance_linear_ode_test.R
@@ -0,0 +1,61 @@
+dimension <- 5
+time_point <- 0:10
+ITER <- 1
+seed <- 0
+
+print('Generate simulation data:')
+repeat
+{
+  print(seed)
+  set.seed(seed)
+  source('linear_ode_generation/linear_ode_generation.R')
+  linear_ode <- linear_ode_generation (
+    dimension
+    , time_point
+    , row_column_permutation = FALSE
+    , intercept = c(0,1e-2)
+  )
+  source('linear_ode_generation/check.linear_ode.R')
+  ode_property <- check.linear_ode(linear_ode)
+  if ( ode_property$max_correlation<0.3 && ode_property$vif<5 )
+  {
+    break
+  }
+  seed <- seed + ITER
+}
+print('Start of iteration:')
+print(seed)
+
+noise_level <- 0
+#noise_level <- 0.3
+noise <- matrix (
+  rnorm (
+    length(time_point) * dimension
+    , 0
+    , noise_level * sd(linear_ode$observation[,-1])
+  )
+  , length(time_point)
+  , dimension
+)
+observation_noise <- linear_ode$observation
+observation_noise[,-1] <- observation_noise[,-1] + noise
+
+initial <- numeric(dimension)
+
+source('separable_ls.R')
+estimation <- separable_ls (
+  observation_noise
+#  , upper_bound_complex = 1+1i
+#  , lower_bound_complex = -1-1i
+  , intercept = !is.null(linear_ode$intercept)
+#  , method = 'dfo'
+  , verbose = 1
+)
+
+source('variance_linear_ode.R')
+variance <-
+  variance_linear_ode (
+    estimation
+    , linear_ode$observation
+    , method = 'fim'
+  )
--- variance_linear_ode.R
+++ variance_linear_ode.R
@@ -2,13 +2,68 @@
 # estimation.
 
 variance_linear_ode <- function (
+  estimation
+  , observation
+  , method = 'fim'
 )
 
 {
 
+dimension <- nrow(estimation$coefficient_est)
+intercept <- !is.null(estimation$intercept_est)
+
+# Method "fim"#{{{
+
+source('gradient.R')
+if ( method=='fim' )
+{
+  ret <- list()
+
+  dof <- (
+    length(observation) - nrow(observation)
+    - dimension * ( dimension + 1 )
+  )
+  if ( intercept )
+  {
+    dof <- dof - dimension
+  }
+
+  if ( dof<=0 )
+  {
+    stop('Degree of Freedom is not positive.')
+  }
+
+  fim <-
+    .gradient (
+      list (
+        coefficient = estimation$coefficient_est
+        , initial = estimation$curve_est[1,-1]
+        , intercept = estimation$intercept_est
+        , time_point = observation[,1]
+      )
+      , type = 'fim'
+    )
+
+  variance <- diag(solve(fim))
+  variance <- variance * estimation$rss / dof
+
+  ret$coefficient <-
+    matrix (
+      variance[1:(dimension**2)]
+      , dimension
+      , dimension
+    )
+  ret$initial <- variance[(dimension**2+1):(dimension**2+dimension)]
+  if ( intercept )
+  {
+    ret$intercept <- tail(variance,dimension)
+  }
+}
+#}}}
+
 # Return#{{{
 
-return()
+return(ret)
 #}}}
 
 }
