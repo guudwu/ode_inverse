b02998d970b29590ba32c8f4eb08174bb54665ba
GuuD WU <guuuuud@gmail.com>
2013-11-30 19:12:48 -0500

    Add coefficient gradient block in "gradient.R"
    Add block in "gradient.R" to compute the gradient with respect to
    coefficient matrix.
    
    Modify "gradient_test.R" in "test/".
    
    Remove input argument "type".
    Might add back later.

--- gradient.R
+++ gradient.R
@@ -2,36 +2,56 @@
 # Compute gradient of ODE system solution with respect to
 # coefficient matrix, intercept and initial condition, respectively.
 
-# TODO: output type
-
 .gradient <- function (
   linear_ode
-  , type = 'gradient'
 )
 
 {
 
-#ainv <- solve(linear_ode$coefficient)
-#ainv_b <- ainv %*% linear_ode$intercept
+# Initialization#{{{
 
 ret <- list()
 
 dimension <- nrow(linear_ode$coefficient)
 time_point <- linear_ode$observation[,1]
+intercept <- !is.null(linear_ode$intercept)
 
-if ( type=='gradient' )
+gradient_coefficient <-
+  lapply ( 1:(dimension**2) , function(index)
+  {
+    ret <-
+      matrix (
+        0
+        , dimension
+        , length(time_point)
+      )
+    return(ret)
+  } )
+
+gradient_initial <-
+  lapply ( 1:dimension , function(index)
+  {
+    ret <-
+      matrix (
+        0
+        , dimension
+        , length(time_point)
+      )
+    return(ret)
+  } )
+#}}}
+
+# Gradient#{{{
+
+if ( intercept )
 {
-  gradient_initial <-
-    lapply ( 1:dimension , function(index)
-    {
-      ret <-
-        matrix (
-          0
-          , dimension
-          , length(time_point)
-        )
-      return(ret)
-    } )
+  ainv_b <-
+    solve (
+      linear_ode$coefficient
+      , linear_ode$intercept
+    )
+  ainv <-
+    solve ( linear_ode$coefficient )
 }
 
 require('expm')
@@ -42,19 +62,70 @@ lapply ( 1:length(time_point) , function(index)
     expm::expm (
       linear_ode$coefficient * time_point[index]
     )
+
+# Coefficient gradient#{{{
+
+  lapply ( 0:(dimension**2-1) , function(index2)
+  {
+    num_row <- floor(index2/dimension) + 1
+    num_col <- index%%dimension + 1
+
+    direction <- matrix ( 0 , dimension , dimension )
+    direction[num_row,num_col] <- 1
+
+    frechet <-
+      expm::expmFrechet (
+        linear_ode$coefficient * time_point[index]
+        , direction * time_point[index]
+        , expm = FALSE
+      ) $ Lexpm
+
+    if ( intercept )
+    {
+      partial_ainv_b <-
+        solve (
+          linear_ode$coefficient
+          , direction %*% ainv_b
+        )
+    }
+
+    if ( intercept )
+    {
+      gradient_coefficient[[index2+1]][,index] <<- (
+        exp_at %*% partial_ainv_b
+        + frechet %*% ( linear_ode$initial + ainv_b )
+        - partial_ainv_b
+      )
+    }
+    else
+    {
+      gradient_coefficient[[index2+1]][,index] <<-
+        frechet %*% linear_ode$initial
+    }
+
+    return()
+  } )
+#}}}
+
+# Initial condition gradient#{{{
+
   lapply ( 1:dimension , function(index2)
   {
     gradient_initial[[index2]][,index] <<- exp_at[,index2]
     return()
   } )
+#}}}
+
   return()
 } )
-
-ret$gradient_initial <- gradient_initial
+#}}}
 
 # Return#{{{
 
-  return(ret)
+ret$initial <- gradient_initial
+ret$coefficient <- gradient_coefficient
+
+return(ret)
 #}}}
 
 }
--- test/gradient_test.R
+++ test/gradient_test.R
@@ -1,5 +1,5 @@
 set.seed(0)
-dimension <- 9
+dimension <- 5
 time_point <- 0:100
 intercept <- c(0,1e-2)
 
@@ -30,10 +30,43 @@ gradient <-
 
 require('deSolve')
 
-rel_rss <- lapply ( 1:dimension , function(index)
-{
-  step_length <- 1e-5
+step_length <- 1e-5
 
+rel_coefficiet <- lapply ( 0:(dimension**2-1) , function(index)
+{
+  num_row <- floor(index/dimension) + 1
+  num_col <- index%%dimension + 1
+
+  perturb <- matrix ( 0 , dimension , dimension )
+  perturb[num_row,num_col] <- 1
+
+  perturb_res <-
+    ode (
+      linear_ode$initial
+      , linear_ode$observation[,1]
+      , linODE
+      , list (
+          linear_ode$coefficient + step_length * perturb
+          , linear_ode$intercept
+        )
+    ) [,-1]
+
+  ret <-
+    sum (
+      ( perturb_res - linear_ode$observation[,-1]
+        - step_length * t(gradient$coefficient[[index+1]])
+      ) **2
+    ) / sum (
+      perturb_res ** 2
+    )
+
+  return(ret)
+} )
+
+print(rel_coefficiet)
+
+rel_initial <- lapply ( 1:dimension , function(index)
+{
   perturb <- numeric(dimension)
   perturb[index] <- step_length
 
@@ -48,7 +81,7 @@ rel_rss <- lapply ( 1:dimension , function(index)
   ret <-
     sum (
       ( perturb_res - linear_ode$observation[,-1]
-        - step_length * t(gradient$gradient_initial[[index]])
+        - step_length * t(gradient$initial[[index]])
       ) **2
     ) / sum (
       perturb_res ** 2
@@ -57,4 +90,4 @@ rel_rss <- lapply ( 1:dimension , function(index)
   return(ret)
 } )
 
-print(rel_rss)
+print(rel_initial)
